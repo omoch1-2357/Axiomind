ec95f347025c03742fc99e4c72bb6c8d53d5463b
omoch1-2357 <omoch1-2357@users.noreply.github.com>
2025-11-21 15:48:39 +0000

    deploy: fe7d5e8221031d6de1d75d530b3215fdf597be33

--- axiomind_cli/sidebar-items.js
+++ axiomind_cli/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CliError"],"fn":["run"],"mod":["formatters","io_utils","ui","validation"],"struct":["BatchValidationError"]};
+window.SIDEBAR_ITEMS = {"enum":["CliError","Vs"],"fn":["run"],"mod":["formatters","io_utils","ui","validation"],"struct":["BatchValidationError"]};
---
+++ trait.impl/clap_builder/derive/trait.ValueEnum.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["axiomind_cli",[["impl ValueEnum for <a class=\"enum\" href=\"axiomind_cli/enum.Vs.html\" title=\"enum axiomind_cli::Vs\">Vs</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[133]}
